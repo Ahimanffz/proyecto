8d13cc66001e3e18e874e8f06ebe3239a77965d3
Ahiman <133602839+Ahimanffz@users.noreply.github.com>
2026-07-22 21:18:37 -0400

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+// Reemplaza esto con los datos de tu proyecto en la consola de Firebase
+const firebaseConfig = {
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_PROYECTO.firebaseapp.com",
+    projectId: "TU_PROYECTO",
+    storageBucket: "TU_PROYECTO.appspot.com",
+    messagingSenderId: "TU_SENDER_ID",
+    appId: "TU_APP_ID"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exportar servicios
+export const db = getFirestore(app);
+export const auth = getAuth(app); // Estructura preparada para Firebase Authentication
