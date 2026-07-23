ecb14e152a06d568037612431d8f372d594d4567
Ahiman <133602839+Ahimanffz@users.noreply.github.com>
2026-07-22 21:53:29 -0400

    Initialize Firebase and export Firestore and Auth

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,8 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+// Reemplaza esto con los datos de tu proyecto en la consola de Firebase
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAf8X9uXB52Kb6jf8y90MzEBMFahj20dsg",
@@ -8,3 +13,10 @@ const firebaseConfig = {
   appId: "1:234760615906:web:b8b3477098cdc257892bbb",
   measurementId: "G-WEB1FNM75Y"
 };
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exportar servicios
+export const db = getFirestore(app);
+export const auth = getAuth(app); // Estructura preparada para Firebase Authentication
