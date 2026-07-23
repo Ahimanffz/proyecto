4aa2dd1d042c57497b2d44d403d6091fad8caec7
Ahiman <133602839+Ahimanffz@users.noreply.github.com>
2026-07-22 21:20:12 -0400

    Update Firebase configuration with project details

--- js/firebase.js
+++ js/firebase.js
@@ -3,15 +3,16 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
 // Reemplaza esto con los datos de tu proyecto en la consola de Firebase
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_PROYECTO.firebaseapp.com",
-    projectId: "TU_PROYECTO",
-    storageBucket: "TU_PROYECTO.appspot.com",
-    messagingSenderId: "TU_SENDER_ID",
-    appId: "TU_APP_ID"
+  apiKey: "AIzaSyAf8X9uXB52Kb6jf8y90MzEBMFahj20dsg",
+  authDomain: "kyrox-technology.firebaseapp.com",
+  projectId: "kyrox-technology",
+  storageBucket: "kyrox-technology.firebasestorage.app",
+  messagingSenderId: "234760615906",
+  appId: "1:234760615906:web:b8b3477098cdc257892bbb",
+  measurementId: "G-WEB1FNM75Y"
 };
-
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
